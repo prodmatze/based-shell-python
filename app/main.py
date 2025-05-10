04000f19b68c91ae00f634978d8a3bb8c1e91027
matze <104998443+prodmatze@users.noreply.github.com>
2025-05-10 23:46:02 +0300

    print basic error message for unknown command

--- app/main.py
+++ app/main.py
@@ -5,9 +5,10 @@ def main():
     sys.stdout.write("$ ")
     
     #wait for user input
+    u_input = input()
 
-    input()
-
+    error_msg = f"{u_input}: command not found"
+    print(error_msg)
 
 if __name__ == "__main__":
     main()
