d52fd967d9600ed6f025af732c6008105b83833b
matze <104998443+prodmatze@users.noreply.github.com>
2025-05-11 00:00:27 +0300

    break loop before printing error message

--- app/main.py
+++ app/main.py
@@ -1,18 +1,27 @@
 import sys
 
+def parse_command(input):
+    split_input = input.split(" ", 1)
+    command = split_input[0]
+
+    return command
 
 def main():
     
+
     while True:
         sys.stdout.write("$ ")
         #wait for user input
         u_input = input()
+        command = parse_command(u_input)
 
         error_msg = f"{u_input}: command not found"
+
+        if command == "exit":
+            break
+
         print(error_msg)
 
-        if u_input == "exit 0":
-            break
 
 if __name__ == "__main__":
     main()
