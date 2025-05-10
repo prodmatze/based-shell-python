172272cde7f27b472841577b26b10ba7783b2e20
matze <104998443+prodmatze@users.noreply.github.com>
2025-05-11 00:08:43 +0300

    error handling for empty/none param after command

--- app/main.py
+++ app/main.py
@@ -3,7 +3,11 @@ import sys
 def parse_input(input):
     split_input = input.split(" ", 1)
     command = split_input[0]
-    param = split_input[1]
+
+    if len(split_input > 1):
+        param = split_input[1] 
+    else:
+        param = None
 
     return command, param
 
