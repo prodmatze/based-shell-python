f9a244bc6933b8bb7cbc2ba8a1770dd13ca73e94
matze <104998443+prodmatze@users.noreply.github.com>
2025-05-10 23:48:40 +0300

    wrap input in permanent loop and exit if input==exit

--- app/main.py
+++ app/main.py
@@ -2,13 +2,17 @@ import sys
 
 
 def main():
-    sys.stdout.write("$ ")
     
-    #wait for user input
-    u_input = input()
+    while True:
+        sys.stdout.write("$ ")
+        #wait for user input
+        u_input = input()
 
-    error_msg = f"{u_input}: command not found"
-    print(error_msg)
+        error_msg = f"{u_input}: command not found"
+        print(error_msg)
+
+        if u_input == "exit":
+            break
 
 if __name__ == "__main__":
     main()
