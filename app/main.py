4a944d767af5b3f4c2147a7978d25bc4a3e38135
matze <104998443+prodmatze@users.noreply.github.com>
2025-05-12 15:57:25 +0300

    added support for PATH environment variable, checking dir for executables

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 builtin_commands = ["echo", "exit", "type"]
 
@@ -14,21 +15,44 @@ def parse_input(input):
     return command, param
 
 def main():
-    
+
+
+    paths = os.environ.get("PATH", "").split(":")
 
     while True:
         sys.stdout.write("$ ")
-        #wait for user input
-        u_input = input()
-        command, param = parse_input(u_input)
 
+        #wait for user input
+        try:
+            u_input = input().strip()           #strip to avoid errors with extra whitespaces (e.g " echo hello", "echo hello ")
+        except EOFError:                        #CTRL-D (exit shell)
+            print("BYE BYE")
+            break
+        except KeyboardInterrupt:               #CTRL-C 
+            print("")
+            continue
+
+        command, param = parse_input(u_input)
         error_msg = f"{command}: command not found"
+
+        #handle empty input -> continue to next iteration if user just presses enter with no input
+        if command == "":
+            continue
 
         match command:
             case "echo":
                 print(param)
 
             case "type":
+                if paths:
+                    for path in paths:
+                        if os.path.isfile(path) and os.access(path, os.X_OK):
+                            print(f"{param} is {path}")
+                            continue
+                        else:
+                            print(f"{param}: not found")
+                            continue
+
                 if param in builtin_commands:
                     print(f"{param} is a shell builtin")
                 else:
